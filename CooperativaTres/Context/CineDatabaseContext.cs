dde973f3df80e9843f5f9fb19794df5b86e6109c
Marce <marcebauer96@gmail.com>
2022-11-02 20:52:53 -0300

    se agregó context

---
+++ CooperativaTres/Context/CineDatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CooperativaTres.Models;
+
+namespace CooperativaTres.Context
+{
+    public class CineDatabaseContext : DbContext
+    {
+        public CineDatabaseContext(DbContextOptions<CineDatabaseContext> options)
+: base(options)
+        {
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
